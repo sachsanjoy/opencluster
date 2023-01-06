87a6d85030215bf8c84d64384a83f4472554c51d
Sachu Sanjayan <sachusanjayan@gmail.com>
2023-01-06 16:19:46 +0100

    up

--- plot_cluster_member.py
+++ plot_cluster_member.py
@@ -8,6 +8,9 @@ pt = 0.5 #using a probbaility filter of 90%
 print('Almost certain members : ', len(df[df.gmm_prob>pt]))
 dngc = df[df.gmm_prob>pt]
 
+#print('RV',dngc.radial_velocity.mean())
+#print('RVerr',dngc.radial_velocity.std()/np.sqrt(len(dngc)))
+
 # PLOTING CMD BINARIES
 plt.figure(figsize=(10,6))
 plt.rc('xtick', labelsize=12)
